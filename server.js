32f8ce9e12dcb6b645393358c48d46ee42410544
Roger-Takeshita <roger.takeshita@gmail.com>
2020-01-15 23:47:09 -0500

    Installed and configured .env, method-override (CRUD), mongoose, passport, google oauth

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');                                   //! 3 - Require session package below cookie-parser
+const passport = require('passport');                                         //! 4 - Require passport package -  AFTER session (alwyas)
 const logger = require('morgan');
-const methodOverrider = require('method-override');                          //! 1 - Require method-override to make CRUD opereations (below logger)
+const methodOverrider = require('method-override');                           //! 1 - Require method-override to make CRUD opereations (below logger)
 
 const indexRouter = require('./routes/index');
+
+require('dotenv').config();                                                   //! 5 - Require Dotenv
+require('./config/database');                                                 //! 6 - Require MongoDB Database
+require('./config/passport');                                                 //! 7 - Require our passport file after database
 
 const app = express();
 
@@ -16,8 +22,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({                                                             //! 8 - Session middleware, mount AFTER cookieParser();
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: true,
+}));
+app.use(passport.initialize());                                               //! 9 - Passport middleware, mount AFTER session (always)
+app.use(passport.session());                                                  //! 10- Passport middleware, mount AFTER session (always)
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverrider('_method'));                                          //! 2 - Add method-override middleware so que can make a Query String.
+app.use(methodOverrider('_method'));                                         //! 2 - Add method-override middleware so que can make a Query String.
 
 app.use('/', indexRouter);
 
