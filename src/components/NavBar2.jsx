946448e2fbaab111a394b409610818db3804e6c9
Roger Takeshita <roger.takeshita@gmail.com>
2020-03-06 02:37:44 -0500

    Update Navbar
    - Fixed the response
    - Add a blur for the background
    - Add a hook to listen to expand or colapse the menu (once clicked)

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,22 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap'
 import { useSpring, animated } from 'react-spring';
 import Scrollspy from 'react-scrollspy';
 
 const NavBar2 = (props) => {
 
-    // const effect = useSpring({
-    //     from: {
-    //         opacity: 0
-    //     },
-    //     to: {
-    //         opacity: 1
-    //     },
-    //     config: {
-    //         duration: 5000,
-    //         clamp: true
-    //     }
-    // })
+    const [expanded, setExpanded] = useState(false);
 
     const effectMenu = useSpring({
         from: {
@@ -32,18 +21,18 @@ const NavBar2 = (props) => {
     })
 
     return (
-        <Navbar bg="none" expand="lg" fixed="top" className="container">
+        <Navbar bg="none" expand="lg" fixed="top" className="container" expanded={expanded}>
             {/* <Navbar.Brand href="#home"><animated.div className="logo" style={effect}>Roger Takeshita</animated.div></Navbar.Brand> */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
             <Navbar.Collapse id="basic-navbar-nav">
                 <animated.div style={effectMenu}>
                     <Nav className="mr-auto">
                         <Scrollspy className="scrollspy-menu" items={['menu-about-me', 'menu-skills', 'menu-projects', 'menu-education', 'menu-contact-me']} currentClassName="menu-now">
-                            <li><Nav.Link href="#menu-about-me">About Me</Nav.Link></li>
-                            <li><Nav.Link href="#menu-skills">Skills</Nav.Link></li>
-                            <li><Nav.Link href="#menu-projects">Projects</Nav.Link></li>
-                            <li><Nav.Link href="#menu-education">Education</Nav.Link></li>
-                            <li><Nav.Link href="#menu-contact-me">Contact Me</Nav.Link></li>
+                            <li><Nav.Link onClick={() => setExpanded(false)} href="#menu-about-me">About Me</Nav.Link></li>
+                            <li><Nav.Link onClick={() => setExpanded(false)} href="#menu-skills">Skills</Nav.Link></li>
+                            <li><Nav.Link onClick={() => setExpanded(false)} href="#menu-projects">Projects</Nav.Link></li>
+                            <li><Nav.Link onClick={() => setExpanded(false)} href="#menu-education">Education</Nav.Link></li>
+                            <li><Nav.Link onClick={() => setExpanded(false)} href="#menu-contact-me">Contact Me</Nav.Link></li>
                         </Scrollspy>
                     </Nav>
                 </animated.div>
