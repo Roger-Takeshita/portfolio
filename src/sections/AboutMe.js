8ed6bdf5d36c2dbcc190e75c974b2f9391712597
Roger Takeshita <roger.takeshita@gmail.com>
2020-03-11 02:01:44 -0400

    update brand statement

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -5,15 +5,22 @@ const AboutMe = (props) => {
     return (
         <section className="section-about-me container" id="menu-about-me">
             <div className="profile">
-                <div>   
-                    <img src={profilePic} alt="profile" id="profile-pic"/>
+                <div>
+                    <img src={profilePic} alt="profile" id="profile-pic" />
                 </div>
                 <div className="profile-msg">
-                    <p>Curious and detail-oriented, I tend to approach problems with creativity and efficiency first.<br/> My background in engineer has allowed me to develop strong problem-solving skills and the love for automating repetitive tasks.</p>
+                    <p>
+                        Curious and detail-oriented, I approach problems with
+                        creativity and efficiency.
+                        <br />
+                        My background in engineering has allowed me to develop
+                        strong problem-solving skills and the love for
+                        automating tasks.
+                    </p>
                 </div>
             </div>
         </section>
     );
-}
+};
 
 export default AboutMe;
